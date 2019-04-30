6d93c243fa34ccd0b7e445cb11c66ebbc3101127
Matthew Fishman <feesh96@vt.edu>
2019-04-30 13:59:33 -0400

    Extension uses AllSides Org Data

--- Extension/main.js
+++ Extension/main.js
@@ -6,6 +6,7 @@ var gauge;
 var description;
 var bias_category;
 var bias_percentage;
+var orgBias;
 
 chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
    function(tabs){
@@ -17,14 +18,36 @@ chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}
           data: url,
           success: function(response) {
               console.log(response);
+              var json = $.parseJSON(response);
               description = document.getElementById('return_text');
-              description.innerHTML = response;
+              description.innerHTML = json['text'];
+
+              orgBias = json['orgBias'];
               bias_category = document.getElementById('bias_category');
-              bias_category.innerHTML = "Center-Left";
               bias_percentage = document.getElementById('bias_percentage');
-              bias_percentage.innerHTML = "(30% Left)";
               gauge = document.getElementById('gauge_indicator');   // Moves gauge indicator
-              gauge.style.right = 199+'px';   //Scale: 3% = full right, 94% = full left
+
+              if (orgBias == 'left') {
+                  bias_category.innerHTML = 'Left'
+                  bias_percentage.innerHTML = "(80% Left)";
+                  gauge.style.right = 270+'px';   //Scale: 3% = full right, 94% = full left
+              } else if (orgBias == 'left-center') {
+                  bias_category.innerHTML = 'Center-Left'
+                  bias_percentage.innerHTML = "(30% Left)";
+                  gauge.style.right = 199+'px';   //Scale: 3% = full right, 94% = full left
+              } else if (orgBias == 'center') {
+                  bias_category.innerHTML = 'Center'
+                  bias_percentage.innerHTML = "(Minimal Bias)";
+                  gauge.style.right = 153+'px';   //Scale: 3% = full right, 94% = full left
+              } else if (orgBias == 'right-center') {
+                  bias_category.innerHTML = 'Center-Right'
+                  bias_percentage.innerHTML = "(30% Right)";
+                  gauge.style.right = 107+'px';   //Scale: 3% = full right, 94% = full left
+              } else if (orgBias == 'right') {
+                  bias_category.innerHTML = 'Right'
+                  bias_percentage.innerHTML = "(80% Right)";
+                  gauge.style.right = 37+'px';   //Scale: 3% = full right, 94% = full left
+              }
           },
           error: function(error) {
               console.log(error);
--- Extension/main.py
+++ Extension/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
-import requests
+import requests, json
 import justext
+# from newsplease import NewsPlease   # can use newsplease for author/title data
 
 app = Flask(__name__)
 
@@ -9,20 +10,42 @@ def hello():
     return "Welcome to Python Flask!"
 
 
-def runModel(text):
-    return text;
+def runModel(resp):
+    return resp;
     # return "30"
 
 @app.route("/getBias")
 def getBias():
     url = request.args.get('url') #if key doesn't exist, returns None
+
+    # get org data
+    url_obj = requests.urllib3.util.parse_url(url)
+    baseURL = url_obj.host
+    with open('AllSidesFinal.json') as f:
+        data = json.load(f)
+
+    bias = data["bias"][baseURL]
+
+    #get main text
     response = requests.get(url)
     paragraphs = justext.justext(response.content, justext.get_stoplist("English"))
     str = ''
     for paragraph in paragraphs:
       if not paragraph.is_boilerplate:
           str += ("\n" + paragraph.text)
-    return runModel(str)
+
+
+
+    response = {}
+    response['text'] = str
+    response['orgBias'] = bias
+    response['baseURL'] = baseURL
+    json_data = json.dumps(response)
+
+    return runModel(json_data)
+
+    # response = NewsPlease.from_url(url)
+    # return response.text
 
 if __name__ == "__main__":
     app.run(debug=True)
